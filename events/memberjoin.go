06365babb7e78958254d4ad87d52550c05b1b789
nwoik <71194216+nwoik@users.noreply.github.com>
2024-05-02 22:44:28 +0100

    Fixed null pointer issue

--- events/memberjoin.go
+++ events/memberjoin.go
@@ -7,6 +7,7 @@ import (
 )
 
 func MemberJoin(session *discordgo.Session, member *discordgo.GuildMemberAdd) {
+	fmt.Printf("Member joined the server: %s\n", member.User.Username)
 
 	channel, err := session.UserChannelCreate(member.User.ID)
 	if err != nil {
@@ -20,9 +21,7 @@ func MemberJoin(session *discordgo.Session, member *discordgo.GuildMemberAdd) {
 		return
 	}
 
-	_, err = session.ChannelMessageSend(channel.ID, fmt.Sprintf("Welcome to the %s server!\nPlease register with the bot using the `/register` command in the server.\nTo register with the clan please make a ticket in <#1225156608145887243>.", guild.Name))
-	if err != nil {
-		fmt.Println("Error sending direct message: ", err)
-		return
-	}
+	session.ChannelMessageSend(channel.ID, fmt.Sprintf("Welcome to the %s server!\n"+
+		"Please register with the bot using the `/register` command in the server.\n"+
+		"To register with the clan please make a ticket in <#1225156608145887243>.", guild.Name))
 }
